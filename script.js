f7d8a42c8b8359961b73f8f34362d8da0d6c1b02
marco987 <pacemarco87@gmail.com>
2019-07-12 08:27:42 +0200

    Esercizio

--- script.js
+++ script.js
@@ -6,3 +6,46 @@
 // - Dare la possibilità all’utente attraverso 3 prompt
 // di aggiungere un nuovo oggetto studente
 // inserendo nell’ordine: nome, cognome e età.
+
+var studente = {
+	"nome" : "marco",
+	"cognome" : "pace",
+	"eta" : "32"
+};
+
+for (var descrizione in studente) {
+	console.log(descrizione + " = " + studente[descrizione]);
+}
+
+// ---
+
+var classe = [
+	{"nome" : "marco",
+	"cognome" : "pace",
+	"eta" : "32"},
+	{"nome" : "angelo",
+	"cognome" : "capasso",
+	"eta" : "32"},
+	{"nome" : "vincenzo",
+	"cognome" : "giorgio",
+	"eta" : "32"},
+	{"nome" : "antonio",
+	"cognome" : "errico",
+	"eta" : "32"}
+];
+
+for (var i = 0; i < classe.length; i++) {
+	console.log(classe[i].nome, classe[i].cognome);
+}
+
+// ---
+
+var inserisci_nome = prompt("Inserisci il tuo nome");
+var inserisci_cognome = prompt("Inserisci il tuo cognome");
+var inserisci_eta = prompt("Inserisci la tua età");
+
+classe.push({
+	"nome" : inserisci_nome,
+	"cognome" : inserisci_cognome,
+	"eta" : inserisci_eta
+})
